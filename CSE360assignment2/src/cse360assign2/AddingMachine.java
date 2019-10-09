a9ad1c7496dd308618da64781958e59d5107e19e
Ashwath <rashwath55@gmail.com>
2019-10-09 14:36:33 -0700

    Added javadoc format to the AddindMachine.java file

--- CSE360assignment2/src/cse360assign2/AddingMachine.java
+++ CSE360assignment2/src/cse360assign2/AddingMachine.java
@@ -1,4 +1,19 @@
+/**
+ * @author Ashwath Reddy Koppala
+ * @classID CSE360
+ * @Assignment 2
+ * @Descricption This file contains a package cse360assign2 and within the package there is a class AddingMachine.
+ */
 package cse360assign2;
+/**
+ * @Description
+ * This class AddingMachine has an attribute total(int),Constructor and methods getTotal (),add (value),
+ * subtract (value),String toString (),clear().
+ * @Attribute total
+ * @constructor  AddingMachine ()
+ * @methods getTotal (),add (value),
+ * subtract (value),String toString (),clear().
+ */
 
 public class AddingMachine {
 
@@ -7,22 +22,39 @@ public class AddingMachine {
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
 	}
-	
+	/**
+	 * @Description This method is used to return the current total.
+	 * @return integer value of 'total'.
+	 */
 	public int getTotal () {
 		return 0;
 	}
-	
+	/**
+	 * @Description This function adds the given parameter 'value' to 'total'.
+	 * @param value
+	 */
 	public void add (int value) {
 		
 	}
-	
+	/**
+	 * @Description This function subtracts the given parameter 'value' from 'total'.
+	 * @param value
+	 */
 	public void subtract (int value) {
 		
 	}
+	/**
+	 * @Description This function is used to return the string which keeps track of
+	 * all transactions that took place.
+	 * @return String
+	 */
 		
 	public String toString () {
 		return "";
 	}
+	/**
+	 * @Description This function is used to clear the string
+	 */
 
 	public void clear() {
 	
